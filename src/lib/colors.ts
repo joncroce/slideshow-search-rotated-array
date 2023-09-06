ac70d5fa30135595f10bdd3dea617fd6ce9294c7
Jon Croce <crocedev@gmail.com>
2023-09-06 18:48:32 -0400

    create colors object to reference by usance

---
+++ src/lib/colors.ts
@@ -0,0 +1,11 @@
+const lightblue = '#38BDF8'; // tw light-blue-400
+const green = '#22C55E'; // tw green-500
+const fuschia = '#E879F9'; // tw-fuschia-400
+const lightgray = '#CCCCCC';
+
+export default {
+	searchRange: lightblue,
+	outOfRange: lightgray,
+	result: green,
+	mid: fuschia,
+};
