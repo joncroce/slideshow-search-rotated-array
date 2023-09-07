70ebde9b4ba5be556239b317e29b3e2315837e2f
Jon Croce <crocedev@gmail.com>
2023-09-07 01:52:38 -0400

    add color and fix typo in color name

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,12 +1,13 @@
 const lightblue = '#38BDF8'; // tw light-blue-400
 const green = '#22C55E'; // tw green-500
-const fuschia = '#E879F9'; // tw-fuschia-400
+const fuchsia = '#E879F9'; // tw-fuschia-400
 const lightgray = '#CCCCCC';
 
 export default {
 	searchRange: lightblue,
 	outOfRange: lightgray,
 	result: green,
+	mid: fuchsia,
 	highlight: green,
-	mid: fuschia,
+	target: fuchsia,
 };
