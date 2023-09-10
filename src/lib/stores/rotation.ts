13c7fa8eadb26259b4f36b2d30a2464711818a61
Jon Croce <crocedev@gmail.com>
2023-09-10 11:24:36 -0400

    extract array utility functions

--- src/lib/stores/rotation.ts
+++ src/lib/stores/rotation.ts
@@ -4,38 +4,13 @@ import { circleSvgReady } from '@stores/circle';
 import colors from '@lib/colors';
 import type { BinarySearchState } from '@lib/types';
 
-function createRandomAscendingArrayOfDistinctValues(
-	size: number = 20,
-	min: number = 0,
-	max: number = 60
-) {
-	if (min > max) {
-		throw new Error("Minimum can't be greater than maximum.");
-	}
-
-	if (max - min + 1 < size) {
-		throw new Error(
-			'Insufficient values in range to create an array of that size.'
-		);
-	}
-
-	const set = new Set(Array.from({ length: max - min + 1 }, (_, i) => i + min));
-	const result: Array<number> = [];
-
-	while (result.length < size) {
-		const setSize = set.size;
-		const randomIndex = Math.floor(Math.random() * setSize);
-		const value = Array.from(set.values()).at(randomIndex);
-
-		result.push(value);
-		set.delete(value);
-	}
-
-	return result.sort((a, b) => a - b);
-}
+import {
+	createRandomAscendingArrayOfDistinctValues,
+	modifyArrayToContainDuplicateValues,
+} from '@lib/utils/array';
 
 export const array = writable<Array<number>>(
-	createRandomAscendingArrayOfDistinctValues()
+	createRandomAscendingArrayOfDistinctValues(20, 0, 60)
 );
 
 const wrapProgress = gsap.utils.wrap(0, 1);
@@ -896,30 +871,9 @@ function buildModifiedWithDuplicatesTargetSearchStates(
 	return states;
 }
 
-export const arrayWithDuplicates = derived([array], ([$array]) => {
-	if (!$array || $array.length === 0) {
-		return [];
-	}
-
-	const arraySize = $array.length;
-	const minDuplicateRatio = 0.3;
-	const valuesToDuplicate = Array.from(
-		{ length: Math.ceil((arraySize * minDuplicateRatio) / 2) },
-		() => $array[Math.floor(Math.random() * arraySize)]
-	);
-	const result: Array<number> = valuesToDuplicate.concat(valuesToDuplicate);
-
-	for (let i = result.length; i < arraySize; i++) {
-		const value = $array[Math.floor(Math.random() * arraySize)];
-
-		result[i] = value;
-	}
-
-	result.sort((a, b) => a - b);
-
-	return result;
-});
-
+export const arrayWithDuplicates = derived([array], ([$array]) =>
+	modifyArrayToContainDuplicateValues($array, 0.3)
+);
 export const modifiedWithDuplicatesTarget = writable<number>(null);
 export const modifiedWithDuplicatesTargetSearchAnimationProgress = writable(-1);
 export const modifiedWithDuplicatesTargetSearchAnimation = derived(
---
+++ src/lib/utils/array.ts
@@ -0,0 +1,55 @@
+export function createRandomAscendingArrayOfDistinctValues(
+	size: number = 20,
+	min: number = 0,
+	max: number = 60
+) {
+	if (min > max) {
+		throw new Error("Minimum can't be greater than maximum.");
+	}
+
+	if (max - min + 1 < size) {
+		throw new Error(
+			'Insufficient values in range to create an array of that size.'
+		);
+	}
+
+	const set = new Set(Array.from({ length: max - min + 1 }, (_, i) => i + min));
+	const result: Array<number> = [];
+
+	while (result.length < size) {
+		const setSize = set.size;
+		const randomIndex = Math.floor(Math.random() * setSize);
+		const value = Array.from(set.values()).at(randomIndex);
+
+		result.push(value);
+		set.delete(value);
+	}
+
+	return result.sort((a, b) => a - b);
+}
+
+export function modifyArrayToContainDuplicateValues(
+	array: Array<number> = [],
+	minDuplicateRatio: number = 0.3
+) {
+	if (array.length === 0) {
+		return [];
+	}
+
+	const arraySize = array.length;
+	const valuesToDuplicate = Array.from(
+		{ length: Math.ceil((arraySize * minDuplicateRatio) / 2) },
+		() => array[Math.floor(Math.random() * arraySize)]
+	);
+	const result: Array<number> = valuesToDuplicate.concat(valuesToDuplicate);
+
+	for (let i = result.length; i < arraySize; i++) {
+		const value = array[Math.floor(Math.random() * arraySize)];
+
+		result[i] = value;
+	}
+
+	result.sort((a, b) => a - b);
+
+	return result;
+}
