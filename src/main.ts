84cae39cb7cf7a6695188be0fc91b475b0f4bb9e
Jon Croce <crocedev@gmail.com>
2023-09-12 22:58:23 -0400

    hide target value input warning text div

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Slides from './slides.svelte';
 import '@styles/tailwind.css';
 import 'carbon-components-svelte/css/g90.css';
+import '@styles/carbon-override.css';
 
 const app = new Slides({
 	target: document.getElementById('app'),
